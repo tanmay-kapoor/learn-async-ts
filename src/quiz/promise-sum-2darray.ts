21887d1dd3bd549ea5a46517bade77aa86ff200e
tanmaykapoor <tanmay.skater@gmail.com>
2024-10-18 22:22:51 -0400

    complete second task

--- src/quiz/promise-sum-2darray.ts
+++ src/quiz/promise-sum-2darray.ts
@@ -3,3 +3,20 @@ const array2D_1 = [
     [4, 5, 6],
     [7, 8, 9]
 ];
+
+const promises: Promise<number>[] = array2D_1.map((row: number[]) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const sum = row.reduce((acc, val) => acc + val, 0);
+            resolve(sum);
+        }, 0);
+    })
+});
+
+Promise.all(promises)
+    .then((sums: number[]) => {
+        console.log('sums:', sums);
+    })
+    .catch(err => {
+        console.error('Error:', err);
+    });
